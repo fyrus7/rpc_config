f7eff67df2736ecae2acf1347ecd38a81442c865
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2026-04-18 15:42:06 +0800

    Fix typo in API_URL assignment

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-wwindow.API_URL = "https://script.google.com/macros/s/AKfycbyMWRx5hzWxravta8UAKrrfK7kIqV-kzjsGETE7pjnGwg7KwQULS0-6gFE7y_8-LXl2tA/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbyMWRx5hzWxravta8UAKrrfK7kIqV-kzjsGETE7pjnGwg7KwQULS0-6gFE7y_8-LXl2tA/exec";
 
 // window.API_URL = "https://script.google.com/macros/s/AKfycbyrAadWhvTwQK6o5UA_1T-4SlE3UoYxkxBQ_NRU2QwDMA0CdowPs0WiRzXL02Ppt0JW/exec";
