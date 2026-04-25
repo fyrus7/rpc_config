585f4e6b39a99edde76c57567baf87df4fa83625
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2026-04-25 10:54:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.API_URL = "https://script.google.com/macros/s/AKfycbyrAadWhvTwQK6o5UA_1T-4SlE3UoYxkxBQ_NRU2QwDMA0CdowPs0WiRzXL02Ppt0JW/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbypRxQvetFGFXqQRxTPA686yAOuHi1Nz8vWMvdoIeHEh6bx0wLKmZcjamd4Afqm9ShG/exec";
